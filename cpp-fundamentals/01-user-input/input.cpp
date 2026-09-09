10fea58f1d47d7b577efbb9c66ab1717d983c7dc
Divyaansh <divyaansh.vashishat@student.uts.edu.au>
2026-09-10 01:04:01 +1000

    Fix include directive and add newline to output

--- cpp-fundamentals/01-user-input/input.cpp
+++ cpp-fundamentals/01-user-input/input.cpp
@@ -1,4 +1,4 @@
-nclude <iostream>
+#include <iostream>
 #include <limits>
 int main(){
        	std::cout << "What is your age?\n";
@@ -7,6 +7,6 @@ int main(){
         std::cout << "What is your favourite number?\n";
 	std::cin >> number;
 	int max_int = std::numeric_limits<int>::max();
-	std::cout << "Your age is " << age << " and your favourite number is " << number << " and did you know that the highest number for an integer in C++ is " << max_int;
+	std::cout << "Your age is " << age << " and your favourite number is " << number << " and did you know that the highest number for an integer in C++ is " << max_int << "?\n";
 }
 
