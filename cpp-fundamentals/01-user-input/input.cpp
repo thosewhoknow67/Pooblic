096df743cf2a934c67008cfb8e4c6244d7b6fe36
Divyaansh <divyaansh.vashishat@student.uts.edu.au>
2026-09-10 00:35:14 +1000

    Add double input and numeric limits example

---
+++ cpp-fundamentals/01-user-input/input.cpp
@@ -0,0 +1,12 @@
+nclude <iostream>
+#include <limits>
+int main(){
+       	std::cout << "What is your age?\n";
+       	double  age{}, number{};
+	std::cin >> age; 
+        std::cout << "What is your favourite number?\n";
+	std::cin >> number;
+	int max_int = std::numeric_limits<int>::max();
+	std::cout << "Your age is " << age << " and your favourite number is " << number << " and did you know that the highest number for an integer in C++ is " << max_int;
+}
+
