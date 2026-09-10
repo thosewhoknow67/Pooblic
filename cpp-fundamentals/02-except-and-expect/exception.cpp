6993cd5388e626ebfb5eb178b9176887f54b91b3
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-11 00:46:26 +1000

    Handle exceptions with try-catch

--- cpp-fundamentals/02-except-and-expect/exception.cpp
+++ cpp-fundamentals/02-except-and-expect/exception.cpp
@@ -13,8 +13,15 @@ double get_number(std::istream & input){
 }
 
 int main(){
-	std::cout << "Please enter a number: ";
-	double number = get_number(std::cin);
-	std::cout << "Got it, " << number << ", thanks :)\n";
+	std::cout << "Please enter a number: ";	
+	double number{};
 
+	try{
+		number = get_number(std::cin);
+		std::cout << "Got it, " << number << ", thanks :)\n";
+	}
+
+	catch (const std::exception & ex){
+		std::cout << "Something went wrong\n";
+	}
 }
