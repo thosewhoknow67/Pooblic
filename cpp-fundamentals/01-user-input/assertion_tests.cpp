a9de43d16e00f388a72227b82b050060ec0838d2
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-10 17:33:03 +1000

    Add get_number stub with basic test coverage

--- cpp-fundamentals/01-user-input/assertion_tests.cpp
+++ cpp-fundamentals/01-user-input/assertion_tests.cpp
@@ -1,7 +1,18 @@
 #include <cassert>
+#include <sstream>
+#include <istream>
+
+bool get_number(std::istream & input, double & number){
+	number = 1;
+	return true;
+}
 
 void test_code(){
-
+	double value{};
+	std::stringstream forced_input{"1"};
+	const bool ok = get_number(forced_input, value);
+	assert(ok);
+	assert(value == 1);
 }
 
 int main(){
