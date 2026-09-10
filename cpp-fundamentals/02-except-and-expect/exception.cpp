806512c2dc0cfeddd48f7a8f988f0a89100b1707
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-11 00:01:39 +1000

    Add exception throwing to number input

--- cpp-fundamentals/02-except-and-expect/exception.cpp
+++ cpp-fundamentals/02-except-and-expect/exception.cpp
@@ -1,3 +1,20 @@
+#include <istream>
+#include <iostream>
+#include <exception>
+
+double get_number(std::istream & input){
+	double number{};
+	input >> number;
+
+	if(input){
+		return number;
+	}
+	throw std::exception{};
+}
+
 int main(){
+	std::cout << "Please enter a number: ";
+	double number = get_number(std::cin);
+	std::cout << "Got it, " << number << ", thanks :)\n";
 
 }
