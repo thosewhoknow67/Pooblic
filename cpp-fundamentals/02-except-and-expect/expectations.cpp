2245145771592867dfb6c1e48b9f931887b2128b
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-11 01:26:47 +1000

    Add std::expected number handling

--- cpp-fundamentals/02-except-and-expect/expectations.cpp
+++ cpp-fundamentals/02-except-and-expect/expectations.cpp
@@ -1,5 +1,23 @@
 #include <expected>
+#include <string>
+#include <iostream>
+
+std::expected<double, std::string> get_number(std::istring & input){
+	double number{};
+	input >> numeber;
+	if(input){
+		return number;
+	}
+	return std::unexpected{"That's not a number!\n"};
 
 int main(){
-
+	std::cout << "Please enter a number: \n";
+	auto number = get_number(std::cin);
+        if (number.has_value()){
+		std::cout << "Thank you, your number was " << number;
+	}
+	else {
+		std::cout << number.error() << "\n";
+	}
+	
 }
