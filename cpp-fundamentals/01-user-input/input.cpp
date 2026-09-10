0ed7bf19a06ef00f68570c1a58e5ce24016be241
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-10 15:20:19 +1000

    Add input validation with EOF and peek checks

--- cpp-fundamentals/01-user-input/input.cpp
+++ cpp-fundamentals/01-user-input/input.cpp
@@ -6,7 +6,11 @@ int main(){
 	std::cin >> age; 
         std::cout << "What is your favourite number?\n";
 	std::cin >> number;
+	if (!std::cin.eof() && std::cin.peek() != '\n'){
+		std::cout << "There was unused input\n";
+	}
 	int max_int = std::numeric_limits<int>::max();
 	std::cout << "Your age is " << age << " and your favourite number is " << number << " and did you know that the highest number for an integer in C++ is " << max_int << "?\n";
+
 }
 
