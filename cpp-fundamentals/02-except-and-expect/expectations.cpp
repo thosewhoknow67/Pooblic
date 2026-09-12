cd22aea26f9d84a90284525fd0f865fc58a3ceba
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-12 21:17:40 +1000

    Add std::expected error handling to get_number

--- cpp-fundamentals/02-except-and-expect/expectations.cpp
+++ cpp-fundamentals/02-except-and-expect/expectations.cpp
@@ -2,19 +2,19 @@
 #include <string>
 #include <iostream>
 
-std::expected<double, std::string> get_number(std::istring & input){
+std::expected<double, std::string> get_number(std::istream & input){
 	double number{};
 	input >> number;
 	if(input){
 		return number;
 	}
 	return std::unexpected{"That's not a number!\n"};
-
+}
 int main(){
 	std::cout << "Please enter a number: \n";
 	auto number = get_number(std::cin);
         if (number){
-		std::cout << "Thank you, your number was " << number;
+		std::cout << "Thank you, your number was " << number.value();
 	}
 	else {
 		std::cout << number.error() << "\n";
