eb68ca77004e422801c59d8b93c4980bf042c0d0
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-13 01:37:42 +1000

    Add user input for array elements

--- cpp-fundamentals/03-loops-arrays-vectors/arrays.cpp
+++ cpp-fundamentals/03-loops-arrays-vectors/arrays.cpp
@@ -3,8 +3,6 @@
 #include <iostream>
 #include <limits>
 
-void array_experiment(){
-}
 std::expected<double, std::string> get_number(std::istream & input){
 	double number {};
 	input >> number; 
@@ -12,11 +10,26 @@ std::expected<double, std::string> get_number(std::istream & input){
 		return number;
 	}
         input.clear();
-        input.ignore(std::numeric_limits<std:streamsize>::max(),'\n');
+        input.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
         return std::unexpected{"That's not a number"};	
 }
 
 
 int main(){
-	array_experiment();
+	std::cout << "Please enter some numbers:\n "; 
+	std::array<double, 5u> numbers{};
+	size_t count{0u};
+	while (count < numbers.size())
+	{
+		std::cout << '>';
+		auto number = get_number(std::cin);
+		if (number.has_value()){
+			numbers[count] = number.value();
+			std::cout << "Your number was " << number.value() << '\n';
+		}
+		else { 
+			std::cout << number.error() << '\n';	
+		}
+		++count;
+	}
 }
