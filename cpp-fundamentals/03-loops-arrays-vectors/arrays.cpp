7516c2b2299efb478d06d89a17f90a0d4126c685
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-12 22:20:58 +1000

    Add basic std::array experiment

--- cpp-fundamentals/03-loops-arrays-vectors/arrays.cpp
+++ cpp-fundamentals/03-loops-arrays-vectors/arrays.cpp
@@ -1,7 +1,11 @@
 #include <array>
+#include <iostream>
 
 void array_experiment(){
-
+	std::array<double, 5> number{};
+	std::cout << number[0] << '\n';
+	number[0] = 2.5;
+	std::cout << number[0];	
 }
 
 int main(){
