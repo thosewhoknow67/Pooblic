be057e434d67b0fcbf3177692e074bd57b2ad4fb
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-12 21:45:11 +1000

    Add loop for repeated number input

--- cpp-fundamentals/03-loops-arrays-vectors/while_loops.cpp
+++ cpp-fundamentals/03-loops-arrays-vectors/while_loops.cpp
@@ -1,3 +1,25 @@
-int main(){
+#include <expected>
+#include <string>
+#include <iostream>
 
+std::expected<double, std::string> get_number(std::istream & input){
+	double number{};
+	input >> number;
+	if(input){
+		return number;
+	}
+	return std::unexpected{"That's not a number!\n"};
+}
+int main(){
+	std::cout << "Please enter a number: \n";
+	auto number = get_number(std::cin);
+	while (true){
+		if (number){
+			std::cout << "Thank you, your number was " << number.value();
+			break;
+		}
+		else {
+			std::cout << number.error() << "\n";
+		}						
+	}
 }
