1684f18f166081e2d1def9dcd13f497fd5c0a4ce
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-13 01:05:38 +1000

    Add input recovery to expected number handling

--- cpp-fundamentals/03-loops-arrays-vectors/arrays.cpp
+++ cpp-fundamentals/03-loops-arrays-vectors/arrays.cpp
@@ -1,12 +1,21 @@
+#include <expected>
 #include <array>
 #include <iostream>
+#include <limits>
 
 void array_experiment(){
-	std::array<double, 5> number{};
-	std::cout << number[0] << '\n';
-	number[0] = 2.5;
-	std::cout << number[0];	
 }
+std::expected<double, std::string> get_number(std::istream & input){
+	double number {};
+	input >> number; 
+	if (input){
+		return number;
+	}
+        input.clear();
+        input.ignore(std::numeric_limits<std:streamsize>::max(),'\n');
+        return std::unexpected{"That's not a number"};	
+}
+
 
 int main(){
 	array_experiment();
