306495e2df4eee7f22f4d7c057ae10047a09efd9
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-13 11:43:59 +1000

    Add vector initialization and experiment

--- cpp-fundamentals/03-loops-arrays-vectors/vectors.cpp
+++ cpp-fundamentals/03-loops-arrays-vectors/vectors.cpp
@@ -1,4 +1,59 @@
 #include <vector>
+#include <expected>
+#include <array>
+#include <iostream>
+#include <limits>
+#include <string>
 
-int main (){
+std::expected<double, std::string> get_number(std::istream & input){
+	double number {};
+	input >> number;
+	if (input){
+		return number;
+	}
+	input.clear();
+	input.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+	return std::unexpected{"That's not a number"};	
+}
+
+void vector_experiment(){
+	std::vector numbers {0,1};
+	for (const auto number: numbers){
+		std::cout << number << '\n';
+	}
+}
+void show_numbers(const std::array<double,5u> & array){
+	for (const auto number: array){
+		std::cout << number << '\n';
+	}
+}
+
+void max_numbers(const std::array<double,5u> & array){
+	double biggest = array[0];
+	for (const auto number: array){
+		if (number > biggest){
+			biggest = number;
+		}						
+	}
+	std::cout << "The biggest number was " << biggest << '\n';
+}
+
+
+int main(){
+	std::cout << "Please enter some numbers:\n "; 
+	std::array<double, 5u> array_numbers{};
+	size_t count{0u};
+	while (count < array_numbers.size()){
+		std::cout << '>';
+		auto number = get_number(std::cin);
+		if (number.has_value()){
+			array_numbers[count] = number.value();
+		        std::cout << "Your number was " << number.value() << '\n';
+		}
+		else { 
+			std::cout << number.error() << '\n';	
+		}
+		++count;
+	}
+	vector_experiment();
 }
