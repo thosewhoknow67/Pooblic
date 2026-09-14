8a5a392661f41a5a1d1cd411e6f60b6d822356d7
thosewhoknow67 <vashishatdivyaansh1@gmail.com>
2026-09-14 23:29:50 +1000

    Add comments to code

--- cpp-fundamentals/01-user-input/assertion_tests.cpp
+++ cpp-fundamentals/01-user-input/assertion_tests.cpp
@@ -4,6 +4,7 @@
 #include <istream>
 #include <limits>
 
+//get_number function {used no discard to ensure fucntionality}
 [[nodiscard]] bool get_number(std::istream & input, double & number){
 	input >> number;
 	if (input){
@@ -17,12 +18,14 @@
 }
 
 void test_code(){
+	//Valid code 
 	double value{};
 	std::stringstream forced_input{"1"};
 	const bool ok = get_number(forced_input, value);
 	assert(ok);
 	assert(value == 1);
 
+	//Testing for valid failure
 	double value2{};
 	std::stringstream failing_input{"q"};
 	const bool not_ok = get_number(failing_input, value2);
@@ -31,7 +34,8 @@ void test_code(){
 
 int main(){
 	test_code();
-
+	
+	//Simple i/o
 	double number{};
 	std::cout << "Input a number please:\n";
 	const bool ok = get_number(std::cin, number);
